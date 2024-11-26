0491bd6a42c34637e5237bcd8980eba5f854378c
Turbobot <turbobot@vercel.com>
2024-11-26 11:56:34 +0530

    feat(create-turbo): apply official-starter transform

--- apps/ws/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { WebSocket, WebSocketServer } from "ws";
-import { User } from "./User";
-
-const wss = new WebSocketServer({ port: 3001 });
-
-wss.on("connection", (ws: WebSocket) => {
-  console.log("connected");
-  let user = new User(ws);
-});
